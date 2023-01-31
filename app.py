74111d3f9158b8a42cec3805f6490a070a4ec03a
Alexander J. Hernandez Calix <123848740+ajhc101@users.noreply.github.com>
2023-01-31 14:04:39 -0700

    'get_values_in_box' function added

--- app.py
+++ app.py
@@ -32,3 +32,19 @@ def get_values_in_column(grid, col):
             values_in_column.append(number)
 
     return values_in_column
+
+# return a list of numbers already used in a box
+def get_values_in_box(grid, row, col):
+    top_bound = (row // 3) * 3
+    bottom_bound = top_bound + 2
+    left_bound = (col // 3) * 3
+    right_bound = left_bound + 2
+    little_grid = grid[top_bound:(bottom_bound + 1), left_bound:(right_bound + 1)]
+    values_in_box = []
+
+    for r in range(0,3):
+        for c in range(0,3):
+            if little_grid[r][c] != 0:
+                values_in_box.append(little_grid[r, c])
+
+    return values_in_box
